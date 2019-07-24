6d5d5b4cf69a5345bb8f4ed8bd1bc65312a92215
Danilo  Santos <danilo.santos.vieira.contato@gmail.com>
2019-07-24 20:29:16 -0300

    Slots do Bloco de Notas
    Criação e implementacao  do metodo slotSalvar
    Criação dos demais metodos, ams ainda falta implemnetar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,30 +1,138 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :  QMainWindow(parent),  ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	this->ui->setupUi(this);
+
+	//chamada dos connects do QT
+	this->criarConects();
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete this->ui;
+}
+
+void MainWindow::printDebug()
+{
+	qDebug() << "oi passei por aqui" << endl;
 }
 
 void MainWindow::criarConects()
 {
-    connect(ui->actionNovo, SIGNAL(clicked()), this, SLOT(slotNovo()));
-    connect(ui->actionAbrir, SIGNAL(clicked()), this, SLOT(slotAbrir()));
-    connect(ui->actionSalvar, SIGNAL(clicked()), this, SLOT(slotSalvar()));
-    connect(ui->actionDesfazer, SIGNAL(clicked()), this, SLOT(slotDesfazer()));
-    connect(ui->actionCopiar, SIGNAL(clicked()), this, SLOT(slotCopiar()));
-    connect(ui->actionColar, SIGNAL(clicked()), this, SLOT(slotColar()));
-    connect(ui->actionRecortar, SIGNAL(clicked()), this, SLOT(slotRecortar()));
+	/* menu Arquivo*/
+	connect(ui->actionNovo,              SIGNAL(triggered()), this, SLOT(slotNovo()));
+	connect(ui->actionAbrir,             SIGNAL(triggered()), this, SLOT(slotAbrir()));
+	connect(ui->actionSalvar,            SIGNAL(triggered()), this, SLOT(slotSalvar()));
+	connect(ui->actionSalvarComo,        SIGNAL(triggered()), this, SLOT(slotSalvarComo()));
+	connect(ui->actionConfigurarPagina,  SIGNAL(triggered()), this, SLOT(slotConfigurarPagina()));
+	connect(ui->actionImprimir,          SIGNAL(triggered()), this, SLOT(slotImprimir()));
+	connect(ui->actionSair,              SIGNAL(triggered()), this, SLOT(slotSair()));
+	
+
+	/* menu Editar*/
+	connect(ui->actionDesfazer, SIGNAL(triggered()), this, SLOT(slotDesfazer()));
+	connect(ui->actionCopiar,   SIGNAL(triggered()), this, SLOT(slotCopiar()));
+	connect(ui->actionColar,    SIGNAL(triggered()), this, SLOT(slotColar()));
+	connect(ui->actionRecortar, SIGNAL(triggered()), this, SLOT(slotRecortar()));
+	connect(ui->actionExcluir,  SIGNAL(triggered()), this, SLOT(slotExcluir()));
+
+	/* menu Formatar*/
+
+
+	/* menu Exibir*/
+
+
+	/* menu Ajuda*/
+	connect(ui->actionSobre, SIGNAL(triggered()), this, SLOT(slotSobre()));
+
+
+
 }
 
-void slotNovo()
+
+//
+// menu Arquivo
+//
+
+void MainWindow::slotAbrir()
 {
 
 }
+
+//metodo para salvar um arquivo ascii do bloco de notas
+void MainWindow::slotSalvar()
+{
+	//perguntado ao usuario onde ele quer salvar o arquivo
+	QString fileName = QFileDialog::getSaveFileName(this, tr("Salvar Arquivo"), QDir::homePath() , tr("Texto (*.txt)"));
+
+	//Criar Um Arquivo Texto
+	QFile file(fileName);
+	if (!file.open(QIODevice::WriteOnly| QIODevice::Text))
+		return;
+	else {
+
+		QTextStream out(&file);
+		out << this->ui->textEdit->toPlainText();
+
+		// optional, as QFile destructor will already do it:
+		file.close();
+
+	}
+
+}
+
+void MainWindow::slotSalvarComo()
+{
+}
+
+void MainWindow::slotConfigurarPagina()
+{
+}
+
+void MainWindow::slotImprimir()
+{
+}
+
+void MainWindow::slotSair()
+{
+	qApp->quit();
+}
+
+
+//
+// menu Editar
+//
+
+
+void MainWindow::slotDesfazer()
+{
+}
+
+void MainWindow::slotCopiar()
+{
+}
+
+void MainWindow::slotColar()
+{
+}
+
+void MainWindow::slotRecortar()
+{
+}
+
+void MainWindow::slotExcluir()
+{
+}
+
+void MainWindow::slotSobre()
+{
+}
+
+void MainWindow::slotNovo()
+{
+	this->printDebug();
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <cstdio>
+#include <stdio.h>
+#include <QFileDialog>
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -9,17 +18,58 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	explicit MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
+
+	void printDebug();
 
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow *ui;
+
+	// Connects da Classe
+	void criarConects();
+
+private slots:
+
+//
+// menu Arquivo
+//
+
+//slot novo arquivo
+void slotNovo();
+
+//slot para abrir um arquivo
+void slotAbrir();
+
+//slot para salvar um arquivo
+void slotSalvar();
+
+void slotSalvarComo();
+void slotConfigurarPagina();
+void slotImprimir();
+void slotSair();
 
 
-    void criarConects();
+//
+// menu Editar
+//
+
+void slotDesfazer();
+void slotCopiar();
+void slotColar();
+void slotRecortar();
+void slotExcluir();
+
+
+//
+// menu Ajuda
+//
+void slotSobre();
+
+
 };
 
 #endif // MAINWINDOW_H
